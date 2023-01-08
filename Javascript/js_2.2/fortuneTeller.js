0a9026e3c057995d9927211d5fe9273fc4019fbf
shellyalmo <shellyalmo@gmail.com>
2023-01-08 13:38:28 +0200

    first commit

---
+++ Javascript/js_2.2/fortuneTeller.js
@@ -0,0 +1,5 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
+  return `You will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren} children.`;
+}
+
+console.log(tellFortune('chef','Paris','Steve','1'))
