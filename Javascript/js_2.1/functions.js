fe49215335cab5821cbb53f4713e49e97ab957d3
shellyalmo <shellyalmo@gmail.com>
2023-01-08 13:33:22 +0200

    first commit

---
+++ Javascript/js_2.1/functions.js
@@ -0,0 +1,13 @@
+function countryInfo (country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`
+}
+
+let exampleOne = countryInfo('France',67.75, 'Paris')
+
+let exampleTwo = countryInfo('Israel',9.364, 'Jerusalem')
+
+let exampleThree = countryInfo('Greece',10.64, 'Athens')
+
+console.log(exampleOne)
+console.log(exampleTwo)
+console.log(exampleThree)
