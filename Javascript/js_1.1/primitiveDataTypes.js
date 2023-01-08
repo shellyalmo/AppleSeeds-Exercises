3a7699b1b5b919e7d749f94983b00bb9d2d44543
shellyalmo <shellyalmo@gmail.com>
2023-01-08 11:45:08 +0200

    first commit

---
+++ Javascript/js_1.1/primitiveDataTypes.js
@@ -0,0 +1,15 @@
+let stringExample = "hello world";
+let numberExample = 1994;
+let objectExample = ["hi", "there"];
+let boolExample = true;
+let nullExample = null;
+let undefinedExample = undefined;
+const sym = Symbol("foo");
+
+console.log(typeof stringExample);
+console.log(typeof numberExample);
+console.log(typeof objectExample);
+console.log(typeof boolExample);
+console.log(typeof nullExample);
+console.log(typeof undefinedExample);
+console.log(typeof sym);
