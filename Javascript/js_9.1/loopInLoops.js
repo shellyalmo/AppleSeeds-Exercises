04f66cf24f01c5fc39e9604dc2da6ec8c91b9978
shellyalmo <shellyalmo@gmail.com>
2023-01-15 10:52:44 +0200

    first commit

---
+++ Javascript/js_9.1/loopInLoops.js
@@ -0,0 +1,11 @@
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+  }
+}
