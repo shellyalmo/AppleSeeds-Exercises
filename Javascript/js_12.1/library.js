59ac0ee54a20a52e3ec7aa09ed094f83c54d3a20
shellyalmo <shellyalmo@gmail.com>
2023-01-16 14:00:01 +0200

    first commit

---
+++ Javascript/js_12.1/library.js
@@ -0,0 +1,23 @@
+const library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+const canRead = library.filter((book) => {
+  return book.readingStatus === true;
+});
+
+console.log(canRead);
