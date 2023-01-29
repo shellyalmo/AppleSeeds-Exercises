dd5ea500a97139202d8e14a98ea97e7e35b4c6ea
shellyalmo <shellyalmo@gmail.com>
2023-01-29 18:53:56 +0200

    ex22.4 feat car move

---
+++ Javascript/js_22.4/index.js
@@ -0,0 +1,56 @@
+const positions = [null, 1, 1];
+
+/**
+ *
+ * @param {number} playerNum
+ * @param {number} positionNum
+ * @returns {HTMLElement}
+ */
+function getPositionElement(playerNum, positionNum) {
+  return document.querySelector(
+    `#player${playerNum}-race > td:nth-child(${positionNum})`
+  );
+}
+
+/**
+ *
+ * @param {HTMLElement} htmlElement
+ */
+function removeCurrentPosition(htmlElement) {
+  htmlElement.classList.remove("active");
+}
+
+/**
+ *
+ * @param {number} playerNum
+ * @returns {HTMLElement}
+ */
+function findNextPosition(playerNum) {
+  return getPositionElement(playerNum, ++positions[playerNum]);
+}
+
+/**
+ *
+ * @param {HTMLElement} htmlElement
+ */
+function AddPosition(htmlElement) {
+  htmlElement.classList.add("active");
+}
+
+function movePlayer(playerNum) {
+  removeCurrentPosition(getPositionElement(playerNum, positions[playerNum]));
+  AddPosition(findNextPosition(playerNum));
+}
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case " ":
+      movePlayer(1);
+      break;
+    case "ArrowRight":
+      movePlayer(2);
+      break;
+    default:
+      break;
+  }
+});
