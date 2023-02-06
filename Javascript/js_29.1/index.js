662552bd995085fa74e2a78f54e1f4a62b6f096b
shellyalmo <shellyalmo@gmail.com>
2023-02-06 18:59:14 +0200

    ex 29.1

---
+++ Javascript/js_29.1/index.js
@@ -0,0 +1,44 @@
+const getIDs = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([532, 543, 753, 1, 5]);
+    }, 1500);
+  });
+
+const getRecipe = (recipeID) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: "Fresh tomato pasta",
+          publisher: "Pinchas Hodadad",
+        };
+        resolve(`${ID}: ${recipe.title}`);
+      },
+      1500,
+      recipeID
+    );
+  });
+};
+
+// getIDs()
+//   .then((IDs) => {
+//     console.log(IDs);
+//     return getRecipe(IDs[2]);
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//   })
+//   .catch((error) => {
+//     console.log("It is an error!");
+//   });
+async function runAll() {
+  try {
+    const ids = await getIDs();
+    const recipe = await getRecipe(ids[2]);
+    console.log(recipe);
+  } catch (err) {
+    console.log("It is an error!", err);
+  }
+}
+runAll();
